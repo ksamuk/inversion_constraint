05bac210fc7070b769da8b7dc2911d87f823bec9
ksamuk <ksamuk@gmail.com>
2022-04-12 06:54:53 -0700

    polygenic simulations with expanded param values

--- 01_run_simulations.R
+++ 01_run_simulations.R
@@ -40,7 +40,7 @@ remove_previous_data <- function(user_input) {
   
 }
 
-remove_previous_data('y')
+remove_previous_data('n')
 
 
 launch_slim_job <- function(model_params, sim_prefix, out_folder, n_reps = NULL, dryrun = FALSE, array = FALSE){
@@ -126,7 +126,7 @@ recombrate_values <- c(1e-2)
 
 # number of diveregently selected loci (2-50, or 100)
 # current just one setting
-n_div_sel_loci_values <- c(40)
+n_div_sel_loci_values <- c(40, 100)
 
 # all possible combinations of parameters
 # bless u expand.grid
@@ -139,7 +139,7 @@ dry_run <- FALSE
 array <- TRUE
 
 for (i in 1:nrow(param_combos)){
-
+  
   migration_rate <- param_combos[i,][,1]
   popsize <- param_combos[i,][,2]
   npops <- param_combos[i,][,3]
@@ -148,69 +148,69 @@ for (i in 1:nrow(param_combos)){
   mutationrate <- param_combos[i,][,6]
   recombrate <- param_combos[i,][,7]
   ndivselloci <- param_combos[i,][,8]
-
+  
   model_params <- mget(c("migration_rate", "popsize", "npops", "invssup", "sadpative",
                          "mutationrate", "recombrate", "ndivselloci"))
-
+  
   out_folder <- "data/slim_output"
-
-
+  
+  
   launch_slim_job(model_params, sim_prefix = "novel_environment_polygenic", out_folder, n_reps = n_reps, dryrun = dry_run, array = array)
   Sys.sleep(0.1)
 }
 
 
-for (i in 1:nrow(param_combos)){
-
-  migration_rate <- param_combos[i,][,1]
-  popsize <- param_combos[i,][,2]
-  npops <- param_combos[i,][,3]
-  invssup <- param_combos[i,][,4]
-  sadpative <- param_combos[i,][,5]
-  mutationrate <- param_combos[i,][,6]
-  recombrate <- param_combos[i,][,7]
-  ndivselloci <- param_combos[i,][,8]
-
-  model_params <- mget(c("migration_rate", "popsize", "npops", "invssup", "sadpative",
-                         "mutationrate", "recombrate", "ndivselloci"))
-
-  out_folder <- "data/slim_output"
-
-
-  launch_slim_job(model_params, sim_prefix = "novel_environment", out_folder, n_reps = n_reps, dryrun = dry_run, array = array)
-  Sys.sleep(0.1)
-}
-
-
-
-# number of populations
-npops_values <- c(2)
-
-# all possible combinations of parameters
-# bless u expand.grid
-param_combos <- expand.grid(migrate_values, popsize_values, npops_values, 
-                            invssup_values, sadaptive_values, mutationrate_values, recombrate_values, n_div_sel_loci_values)
+# for (i in 1:nrow(param_combos)){
+#   
+#   migration_rate <- param_combos[i,][,1]
+#   popsize <- param_combos[i,][,2]
+#   npops <- param_combos[i,][,3]
+#   invssup <- param_combos[i,][,4]
+#   sadpative <- param_combos[i,][,5]
+#   mutationrate <- param_combos[i,][,6]
+#   recombrate <- param_combos[i,][,7]
+#   ndivselloci <- param_combos[i,][,8]
+#   
+#   model_params <- mget(c("migration_rate", "popsize", "npops", "invssup", "sadpative",
+#                          "mutationrate", "recombrate", "ndivselloci"))
+#   
+#   out_folder <- "data/slim_output"
+#   
+#   
+#   launch_slim_job(model_params, sim_prefix = "novel_environment", out_folder, n_reps = n_reps, dryrun = dry_run, array = array)
+#   Sys.sleep(0.1)
+# }
 
 
-for (i in 1:nrow(param_combos)){
-  
-  migration_rate <- param_combos[i,][,1]
-  popsize <- param_combos[i,][,2]
-  npops <- param_combos[i,][,3]
-  invssup <- param_combos[i,][,4]
-  sadpative <- param_combos[i,][,5]
-  mutationrate <- param_combos[i,][,6]
-  recombrate <- param_combos[i,][,7]
-  ndivselloci <- param_combos[i,][,8]
-  
-  model_params <- mget(c("migration_rate", "popsize", "npops", "invssup", "sadpative", 
-                         "mutationrate", "recombrate", "ndivselloci"))
-  
-  out_folder <- "data/slim_output"
-  
-  
-  launch_slim_job(model_params, sim_prefix = "clim_change", out_folder, n_reps = n_reps, dryrun = dry_run, array = array)
-  Sys.sleep(0.1)
-}
+
+# # number of populations
+# npops_values <- c(2)
+# 
+# # all possible combinations of parameters
+# # bless u expand.grid
+# param_combos <- expand.grid(migrate_values, popsize_values, npops_values, 
+#                             invssup_values, sadaptive_values, mutationrate_values, recombrate_values, n_div_sel_loci_values)
+# 
+# 
+# for (i in 1:nrow(param_combos)){
+#   
+#   migration_rate <- param_combos[i,][,1]
+#   popsize <- param_combos[i,][,2]
+#   npops <- param_combos[i,][,3]
+#   invssup <- param_combos[i,][,4]
+#   sadpative <- param_combos[i,][,5]
+#   mutationrate <- param_combos[i,][,6]
+#   recombrate <- param_combos[i,][,7]
+#   ndivselloci <- param_combos[i,][,8]
+#   
+#   model_params <- mget(c("migration_rate", "popsize", "npops", "invssup", "sadpative", 
+#                          "mutationrate", "recombrate", "ndivselloci"))
+#   
+#   out_folder <- "data/slim_output"
+#   
+#   
+#   launch_slim_job(model_params, sim_prefix = "clim_change", out_folder, n_reps = n_reps, dryrun = dry_run, array = array)
+#   Sys.sleep(0.1)
+# }
 
 #system("tar czf slim_output.tar.gz data")
