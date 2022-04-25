a73ec6cdba255bfd170bd8c62c17737333e9fda1
Kieran Samuk <kieran@kierans-mbp.dyn.ucr.edu>
2022-04-25 16:33:23 -0700

    Supplemental figure code and figures

--- 04_figureS1-S3.R
+++ 04_figureS1-S3.R
@@ -13,7 +13,8 @@ haplo_df <- read.table("data/slim_combined_haplo_df.txt.gz", h = T)
 
 # read in fitness data, censor out gen 1 (pops are initialized in gen 1)
 fitness_df <- read.table("data/slim_combined_fitness_df.txt.gz", h = T) %>%
-  filter(gen > 1)
+  filter(gen > 1) %>%
+  mutate(inv_active = ifelse(grepl("absent", inv_active), "Absent", "Present")) 
 
 ################################################################################
 # compute moving averages for visualization
@@ -26,215 +27,144 @@ mv_avg_optimal_adaptation <- fitness_df %>%
   mutate(seed = 1)
 
 ################################################################################
-# Figure S1: basic scenario
-# this is the 'original' style plot
+# Figure S1: novel environment with parameter combinations
 ################################################################################
 
 mv_avg_absent_basic <- mv_avg_optimal_adaptation %>%
   filter(sim_type == "novel_environment") %>%
-  filter(inv_active == "inversion.absent")
-
-inv_absent_basic <- fitness_df %>%
-  filter(sim_type == "novel_environment") %>%
-  filter(inv_active == "inversion.absent") %>%
-  ggplot(aes(x = gen, y = optimal_adaptation,  color = pop, 
-             group = interaction(pop, seed)))+
-  geom_line(alpha = 0.1, color = "grey", size = 1)+
-  geom_line(data = mv_avg_absent_basic, size = 1)+
-  facet_grid(mig_rate~total_sel_str)+
-  xlab("Generation")+
-  ylab("Scaled Fitness")+ 
-  scale_y_continuous(sec.axis = sec_axis(~ . , name = "Migration rate", breaks = NULL, labels = NULL)) +
-  scale_x_continuous(sec.axis = sec_axis(~ . , name = "Total strength of selection", breaks = NULL, labels = NULL))+
-  theme_bw()+
-  scale_color_brewer(palette = "Set1")
-
-mv_avg_present_basic <- mv_avg_optimal_adaptation %>%
-  filter(sim_type == "novel_environment") %>%
-  filter(inv_active == "inversion.present")
-
-inv_present_basic <- fitness_df %>%
-  filter(sim_type == "novel_environment") %>%
-  filter(inv_active == "inversion.present") %>%
-  ggplot(aes(x = gen, y = optimal_adaptation, color = pop, 
-             group = interaction(seed, pop)))+
-  geom_line(alpha = 0.1, color = "grey", size = 1)+
-  geom_line(data = mv_avg_present_basic, size = 1)+
-  facet_grid(mig_rate~total_sel_str)+
-  xlab("Generation")+
-  ylab("Scaled Fitness")+ 
-  scale_y_continuous(sec.axis = sec_axis(~ . , name = "Migration rate", breaks = NULL, labels = NULL)) +
-  scale_x_continuous(sec.axis = sec_axis(~ . , name = "Total strength of selection", breaks = NULL, labels = NULL))+
-  theme_bw()+
-  scale_color_brewer(palette = "Set1")
-
-
-figure1 <- inv_absent_basic / inv_present_basic
-figure1 + plot_layout(guides = "collect")
-
-
-################################################################################
-# Figure 1: basic scenario
-# this is the 'alternative' style plot, focusing on the inversion contrast
-################################################################################
-
-mv_avg_basic_p3 <- mv_avg_optimal_adaptation %>%
-  filter(sim_type == "novel_environment") %>%
-  filter(pop == "pop3") 
-
-inv_compare_basic_p3 <- fitness_df %>%
-  filter(sim_type == "novel_environment") %>%
-  filter(pop == "pop3") %>%
-  ggplot(aes(x = gen, y = optimal_adaptation,  color = inv_active, 
-             group = interaction(inv_active, seed)))+
-  geom_line(alpha = 0.1, color = "grey", size = 1)+
-  geom_line(data = mv_avg_basic_p3, size = 1)+
-  facet_grid(mig_rate~total_sel_str)+
-  xlab("Generation")+
-  ylab("Scaled Fitness")+ 
-  scale_y_continuous(sec.axis = sec_axis(~ . , name = "Migration rate", breaks = NULL, labels = NULL)) +
-  scale_x_continuous(sec.axis = sec_axis(~ . , name = "Total strength of selection", breaks = NULL, labels = NULL))+
-  theme_bw()+
-  scale_color_brewer(palette = "Set1")
-
-mv_avg_basic_p1p2 <- mv_avg_optimal_adaptation %>%
-  filter(sim_type == "novel_environment") %>%
-  filter(pop == "pop1" | pop == "pop2")
-
-inv_compare_basic_p1p2 <- fitness_df %>%
-  filter(sim_type == "novel_environment") %>%
-  filter(pop == "pop1" | pop == "pop2") %>%
-  ggplot(aes(x = gen, y = optimal_adaptation,  color = inv_active, 
-             group = interaction(inv_active, seed)))+
-  geom_line(alpha = 0.1, color = "grey", size = 1)+
-  geom_line(data = mv_avg_basic_p1p2, size = 1)+
-  facet_grid(mig_rate~total_sel_str)+
-  xlab("Generation")+
-  ylab("Scaled Fitness")+ 
-  scale_y_continuous(sec.axis = sec_axis(~ . , name = "Migration rate", breaks = NULL, labels = NULL)) +
-  scale_x_continuous(sec.axis = sec_axis(~ . , name = "Total strength of selection", breaks = NULL, labels = NULL))+
-  theme_bw()+
-  scale_color_brewer(palette = "Set1")
-
-figure1_alt <- inv_compare_basic_p3 / inv_compare_basic_p1p2
-figure1_alt + plot_layout(guides = "collect")
-
-################################################################################
-# Figure 1: basic scenario
-# this is the plot suggested by kim, combining the two above
-################################################################################
-
-mv_avg_absent_basic <- mv_avg_optimal_adaptation %>%
-  filter(sim_type == "novel_environment") %>%
-  filter(inv_active == "inversion.absent")
+  filter(inv_active == "Absent")
 
 mv_avg_basic_inv_present <- mv_avg_optimal_adaptation %>%
   filter(sim_type == "novel_environment") %>%
-  filter(inv_active == "inversion.present")
+  filter(inv_active == "Present")
 
-inv_absent_basic <- fitness_df %>%
+figureS1 <- fitness_df %>%
   filter(sim_type == "novel_environment") %>%
-  filter(inv_active == "inversion.absent") %>%
-  ggplot(aes(x = gen, y = optimal_adaptation,  color = pop, 
+  ggplot(aes(x = gen, y = optimal_adaptation,  color = pop, linetype = inv_active,
              group = interaction(pop, seed)))+
-  geom_line(alpha = 0.1, color = "grey", size = 1)+
+  geom_line(alpha = 0.1, col = "grey",linetype = "solid")+
   geom_line(data = mv_avg_absent_basic, size = 1)+
-  geom_line(data = mv_avg_basic_inv_present, size = 1, linetype = 2)+
+  geom_line(data = mv_avg_basic_inv_present, size = 1)+
   facet_grid(mig_rate~total_sel_str)+
-  xlab("Generation")+
-  ylab("Scaled Fitness")+ 
-  scale_y_continuous(sec.axis = sec_axis(~ . , name = "Migration rate", breaks = NULL, labels = NULL)) +
-  scale_x_continuous(sec.axis = sec_axis(~ . , name = "Total strength of selection", breaks = NULL, labels = NULL))+
+  labs(x = "Generation", y = "Scaled Fitness", 
+       col = "Population", linetype = "Inversion Presence")+
   theme_bw()+
-  scale_color_brewer(palette = "Set1")
+  theme(strip.background = element_blank())+
+  scale_y_continuous(sec.axis = sec_axis(~ . , name = "Migration rate", 
+                                         breaks = NULL, labels = NULL)) +
+  scale_x_continuous(sec.axis = sec_axis(~ . , name = "Total strength of selection", 
+                                         breaks = NULL, labels = NULL))+
+  scale_color_brewer(palette = "Set1")+
+  scale_linetype_manual(values = c("11", "solid"))
+
+ggsave(figureS1, filename = "figures/FigureS1.png",  
+       height = 7, width = 9)
 
 ################################################################################
-# Figure S1: polygenic scenario
-# this is the 'original' style plot
+# Figure S2: climate change with parameter combinations
 ################################################################################
 
-mv_avg_absent_pg <- mv_avg_optimal_adaptation %>%
-  filter(sim_type == "polygenic") %>%
-  filter(inv_active == "inversion.absent")
+mv_avg_absent_basic <- mv_avg_optimal_adaptation %>%
+  filter(sim_type == "clim_change") %>%
+  filter(inv_active == "Absent")
 
-inv_absent_pg <- fitness_df %>%
-  filter(sim_type == "polygenic") %>%
-  filter(inv_active == "inversion.absent") %>%
-  ggplot(aes(x = gen, y = optimal_adaptation,  color = pop, 
+mv_avg_basic_inv_present <- mv_avg_optimal_adaptation %>%
+  filter(sim_type == "clim_change") %>%
+  filter(inv_active == "Present")
+
+figureS2 <- fitness_df %>%
+  filter(sim_type == "clim_change") %>%
+  ggplot(aes(x = gen, y = optimal_adaptation,  color = pop, linetype = inv_active,
              group = interaction(pop, seed)))+
-  geom_line(alpha = 0.1, color = "grey")+
-  geom_line(data = mv_avg_absent_pg)+
+  geom_line(alpha = 0.1, col = "grey",linetype = "solid")+
+  geom_line(data = mv_avg_absent_basic, size = 1)+
+  geom_line(data = mv_avg_basic_inv_present, size = 1)+
   facet_grid(mig_rate~total_sel_str)+
-  xlab("Generation")+
-  ylab("Scaled Fitness")+ 
-  scale_y_continuous(sec.axis = sec_axis(~ . , name = "Migration rate", breaks = NULL, labels = NULL)) +
-  scale_x_continuous(sec.axis = sec_axis(~ . , name = "Total strength of selection", breaks = NULL, labels = NULL))+
-  theme_bw()
+  labs(x = "Generation", y = "Scaled Fitness", 
+       col = "Population", linetype = "Inversion Presence")+
+  theme_bw()+
+  theme(strip.background = element_blank())+
+  scale_y_continuous(sec.axis = sec_axis(~ . , name = "Migration rate", 
+                                         breaks = NULL, labels = NULL)) +
+  scale_x_continuous(sec.axis = sec_axis(~ . , name = "Total strength of selection", 
+                                         breaks = NULL, labels = NULL))+
+  scale_color_brewer(palette = "Set1")+
+  scale_linetype_manual(values = c("11", "solid"))
 
-mv_avg_present_pg <- mv_avg_optimal_adaptation %>%
-  filter(sim_type == "polygenic") %>%
-  filter(inv_active == "inversion.present")
-
-inv_present_pg <- fitness_df %>%
-  filter(sim_type == "polygenic") %>%
-  filter(inv_active == "inversion.present") %>%
-  ggplot(aes(x = gen, y = optimal_adaptation, color = pop, 
-             group = interaction(seed, pop)))+
-  geom_line(alpha = 0.1, color = "grey")+
-  geom_line(data = mv_avg_present_pg)+
-  facet_grid(mig_rate~total_sel_str)+
-  xlab("Generation")+
-  ylab("Scaled Fitness")+ 
-  scale_y_continuous(sec.axis = sec_axis(~ . , name = "Migration rate", breaks = NULL, labels = NULL)) +
-  scale_x_continuous(sec.axis = sec_axis(~ . , name = "Total strength of selection", breaks = NULL, labels = NULL))+
-  theme_bw()
-
-
-figureS1 <- inv_absent_pg / inv_present_pg
+ggsave(figureS2, filename = "figures/FigureS2.png",  
+       height = 7, width = 9)
 
 ################################################################################
-# Figure S1: polygenic scenario
-# this is the 'alternative' style plot
+# Figure S3: polygenic (loci = 40) with parameter combinations
 ################################################################################
 
-mv_avg_pg_p3 <- mv_avg_optimal_adaptation %>%
+mv_avg_absent_basic <- mv_avg_optimal_adaptation %>%
   filter(sim_type == "polygenic") %>%
-  filter(pop == "pop3")
+  filter(n_loci == 40) %>%
+  filter(inv_active == "Absent")
 
-inv_compare_pg_p3 <- fitness_df %>%
+mv_avg_basic_inv_present <- mv_avg_optimal_adaptation %>%
   filter(sim_type == "polygenic") %>%
-  filter(pop == "pop3") %>%
-  ggplot(aes(x = gen, y = optimal_adaptation,  color = inv_active, 
-             group = interaction(inv_active, seed, pop)))+
-  geom_line(alpha = 0.1, color = "grey", size = 1)+
-  geom_line(data = mv_avg_pg_p3, size = 1)+
+  filter(n_loci == 40) %>%
+  filter(inv_active == "Present")
+
+figureS3 <- fitness_df %>%
+  filter(sim_type == "polygenic") %>%
+  filter(n_loci == 40) %>%
+  ggplot(aes(x = gen, y = optimal_adaptation,  color = pop, linetype = inv_active,
+             group = interaction(pop, seed)))+
+  geom_line(alpha = 0.1, col = "grey",linetype = "solid")+
+  geom_line(data = mv_avg_absent_basic, size = 1)+
+  geom_line(data = mv_avg_basic_inv_present, size = 1)+
   facet_grid(mig_rate~total_sel_str)+
-  xlab("Generation")+
-  ylab("Scaled Fitness")+ 
-  scale_y_continuous(sec.axis = sec_axis(~ . , name = "Migration rate", breaks = NULL, labels = NULL)) +
-  scale_x_continuous(sec.axis = sec_axis(~ . , name = "Total strength of selection", breaks = NULL, labels = NULL))+
+  labs(x = "Generation", y = "Scaled Fitness", 
+       col = "Population", linetype = "Inversion Presence")+
   theme_bw()+
-  scale_color_brewer(palette = "Set1")
+  theme(strip.background = element_blank())+
+  scale_y_continuous(sec.axis = sec_axis(~ . , name = "Migration rate", 
+                                         breaks = NULL, labels = NULL)) +
+  scale_x_continuous(sec.axis = sec_axis(~ . , name = "Total strength of selection", 
+                                         breaks = NULL, labels = NULL))+
+  scale_color_brewer(palette = "Set1")+
+  scale_linetype_manual(values = c("11", "solid"))
 
-mv_avg_pg_p1p2 <- mv_avg_optimal_adaptation %>%
-  filter(sim_type == "polygenic") %>%
-  filter(pop == "pop1" | pop == "pop2")
+ggsave(figureS3, filename = "figures/FigureS3.png",  
+       height = 7, width = 9)
 
-inv_compare_pg_p1p2 <- fitness_df %>%
+################################################################################
+# Figure S4: polygenic (loci = 100) with parameter combinations
+################################################################################
+
+mv_avg_absent_basic <- mv_avg_optimal_adaptation %>%
   filter(sim_type == "polygenic") %>%
-  filter(pop == "pop1" | pop == "pop2") %>%
-  ggplot(aes(x = gen, y = optimal_adaptation,  color = inv_active, 
-             group = interaction(inv_active, seed, pop)))+
-  geom_line(alpha = 0.1, color = "grey", size = 1)+
-  geom_line(data = mv_avg_pg_p1p2, size = 1)+
+  filter(n_loci == 100) %>%
+  filter(inv_active == "Absent")
+
+mv_avg_basic_inv_present <- mv_avg_optimal_adaptation %>%
+  filter(sim_type == "polygenic") %>%
+  filter(n_loci == 100) %>%
+  filter(inv_active == "Present")
+
+figureS4 <- fitness_df %>%
+  filter(sim_type == "polygenic") %>%
+  filter(n_loci == 100) %>%
+  ggplot(aes(x = gen, y = optimal_adaptation,  color = pop, linetype = inv_active,
+             group = interaction(pop, seed)))+
+  geom_line(alpha = 0.1, col = "grey",linetype = "solid")+
+  geom_line(data = mv_avg_absent_basic, size = 1)+
+  geom_line(data = mv_avg_basic_inv_present, size = 1)+
   facet_grid(mig_rate~total_sel_str)+
-  xlab("Generation")+
-  ylab("Scaled Fitness")+ 
-  scale_y_continuous(sec.axis = sec_axis(~ . , name = "Migration rate", breaks = NULL, labels = NULL)) +
-  scale_x_continuous(sec.axis = sec_axis(~ . , name = "Total strength of selection", breaks = NULL, labels = NULL))+
+  labs(x = "Generation", y = "Scaled Fitness", 
+       col = "Population", linetype = "Inversion Presence")+
   theme_bw()+
-  scale_color_brewer(palette = "Set1")
+  theme(strip.background = element_blank())+
+  scale_y_continuous(sec.axis = sec_axis(~ . , name = "Migration rate", 
+                                         breaks = NULL, labels = NULL)) +
+  scale_x_continuous(sec.axis = sec_axis(~ . , name = "Total strength of selection", 
+                                         breaks = NULL, labels = NULL))+
+  scale_color_brewer(palette = "Set1")+
+  scale_linetype_manual(values = c("11", "solid"))
 
-figureS1_alt <- inv_compare_pg_p3 / inv_compare_pg_p1p2
-figureS1_alt + plot_layout(guides = "collect")
+ggsave(figureS3, filename = "figures/FigureS4.png",  
+       height = 7, width = 9)
   
